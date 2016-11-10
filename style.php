8ca14a5bcb678de3637895418a4b9713c47f8bee
Vlad Tokarev <vlad@tokarev.tk>
2016-11-10 18:09:35 +0300

    Output option

--- style.php
+++ style.php
@@ -5,11 +5,12 @@ use \Leafo\ScssPhp\Server;
 use \Leafo\ScssPhp\Compiler;
 
 $config = [];
-$config['dir'] = 'scss';
+$config['dir']    = 'scss';
+$config['output'] = 'Compressed'; // Nested | Expanded | Compact | Compressed | Crunched
 
 $scssCompiler = new Compiler();
 $scssCompiler->setImportPaths($config['dir']);
-$scssCompiler->setFormatter('\Leafo\ScssPhp\Formatter\Crunched');
+$scssCompiler->setFormatter('\Leafo\ScssPhp\Formatter\\' . $config['output']);
 
 $scssServer = new Server($config['dir'], null, $scssCompiler);
 $scssServer->serve();
